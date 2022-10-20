c086b8e3b1c4a7b78b1e38cc73cda150ce78fade
Dennis Mutuma <dennocapi@gmail.com>
2022-10-20 14:51:59 +0300

    Schools page

--- src/components/dashboard/Schools.jsx
+++ src/components/dashboard/Schools.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar } from "./Navbar";
-
+import {SchoolsCard} from './SchoolsCard'
 export const Schools = () => {
   return (
     <div>
@@ -8,15 +8,15 @@ export const Schools = () => {
       <main className="flex-1">
         <div>
           <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8 xl:grid xl:max-w-5xl xl:grid-cols-3">
-            <div className="xl:col-span-2 xl:border-r xl:border-gray-200 xl:pr-8 ">
+            <div className="xl:col-span-5 xl:border-r xl:border-gray-200 xl:pr-8 ">
               <section
                 aria-labelledby="activity-title"
-                className="mt-8 xl:mt-10"
               >
                 <div>
                   <div className="divide-y divide-gray-200">
-                    <div className="pt-6 text-center">
-                      <div className="text-xl font-bold">Schools</div>
+                    <div className="pt-3 text-center">
+                      <div className="text-xl font-bold text-cyan-500 mb-3 border-b p-2">Schools</div>
+                      <SchoolsCard />
                     </div>
                   </div>
                 </div>
