08d4e4d075cfc82d8c229359f341c8b0c9db87b5
Mgrinders <gikunda.dev@gmail.com>
2023-01-30 12:02:00 +0300

    Upload school changes

--- src/components/dashboard/schools/Form.js
+++ src/components/dashboard/schools/Form.js
@@ -261,6 +261,7 @@ export const Form = (props) => {
                       value={country}
                       className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                     >
+                      <option value="">Select country</option>
                       <option>America</option>
                       <option>Canada</option>
                       <option>UK</option>
