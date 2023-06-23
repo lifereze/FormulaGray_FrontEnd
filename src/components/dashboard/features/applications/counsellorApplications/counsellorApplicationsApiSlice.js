703a49ca56f426f3771dc3db26002cece3b90486
Mgrinders <gikunda.dev@gmail.com>
2023-06-23 15:01:41 +0300

    Changing Post to patch method on change password

---
+++ src/components/dashboard/features/applications/counsellorApplications/counsellorApplicationsApiSlice.js
@@ -0,0 +1,87 @@
+import { apiSlice } from "../../../../../features/api/apiSlice";
+
+export const extendedApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getCounsellorApplications: builder.query({
+      query: (stage) => {
+        return {
+          url: "/counselor/applications",
+          method: "POST",
+          credentials: "include",
+          body: {
+            ...stage,
+          },
+        };
+      },
+      transformResponse: (responseData) => {
+        return responseData.applications;
+      },
+      providesTags: (result, error, arg) => {
+        console.log(result);
+        return [
+          { type: "Application", id: "LIST" },
+          result.map(({ _id }) => {
+            return { type: "Application", _id };
+          }),
+        ];
+      },
+    }),
+    getCounsellorSpecificPartnerApplications: builder.query({
+      query: (partnerId, stage) => {
+        return {
+          url: `/counselor/applications/${partnerId}`,
+          method: "POST",
+          credentials: "include",
+          body: {
+            ...stage,
+          },
+        };
+      },
+      transformResponse: (responseData) => {
+        return responseData;
+      },
+      providesTags: (result, error, arg) => {
+        console.log(result);
+        return [
+          { type: "Application", id: "LIST" },
+          result.map(({ _id }) => {
+            console.log(_id);
+            return { type: "Application", _id };
+          }),
+        ];
+      },
+    }),
+    updateCounsellorApplication: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/counselor/application/${id}`,
+        method: "PATCH",
+        credentials: "include",
+        body: data,
+      }),
+
+      invalidatesTags: (result, error, { id: _id }) => {
+        console.log("id", _id);
+        return [{ type: "Application", _id }];
+      },
+    }),
+    deleteCounsellorApplication: builder.mutation({
+      query: ({ id }) => ({
+        url: `/counselor/application/${id}`,
+        method: "DELETE",
+        credentials: "include",
+      }),
+      invalidatesTags: (result, error, { id: _id }) => {
+        console.log("id", _id);
+        return [{ type: "Application", _id }];
+      },
+    }),
+  }),
+});
+
+export const {
+  useGetCounsellorApplicationsQuery,
+  useGetCounsellorSpecificPartnerApplicationsQuery,
+  useDeleteCounsellorApplicationMutation,
+  useUpdateCounsellorApplicationMutation,
+} = extendedApiSlice;
+// returns the query result object
---
+++ src/components/modals/OnboardingModal.js
@@ -0,0 +1,174 @@
+import React, { useState, useEffect } from "react";
+import { AiOutlineClose, AiOutlineArrowRight } from "react-icons/ai";
+import { userStore } from "../../stores";
+import { createUser } from "../../data/api/authenticatedRequests";
+import Spinner from "../utils/Spinner";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function OnboardingModal({ setShowOnboardingModal }) {
+  const initialize = {
+    email: "",
+    firstName: "",
+    lastName: "",
+    phone: "",
+  };
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [student, setStudent] = useState(initialize);
+  const user = userStore((state) => state.user);
+  const handleChange = (input) => {
+    setStudent((prevState) => ({
+      ...prevState,
+      [input.target.name]: input.target.value,
+    }));
+  };
+  const uploadstudent = async () => {
+    if (
+      student.email.length > 0 &&
+      student.firstName.length > 0 &&
+      student.lastName.length > 0 &&
+      student.phone.length > 0
+    ) {
+      toast.success("Details sent successfully.");
+      setShowOnboardingModal(false);
+    } else {
+      setShowOnboardingModal(false);
+    }
+    // if (
+    //   student.email.length > 0 &&
+    //   student.firstName.length > 0 &&
+    //   student.lastName.length > 0
+    // ) {
+    //   setLoading(true);
+    //   try {
+    //     const res = await createUser(student);
+    //     if (res.status == 201) {
+    //       toast.success("student created successfully.");
+
+    //       setShowOnboardingModal(false);
+    //     } else {
+    //       toast.error(res.data.message);
+    //     }
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // }
+    // setLoading(false);
+  };
+
+  return (
+    <>
+      <div
+        className="fixed w-full inset-0 z-75 overflow-hidden flex justify-center items-end md:items-center animated fadeIn faster"
+        style={{ background: "rgba(0,0,0,.6)" }}
+      >
+        <div className="shadow-xl modal-container bg-white text-black w-full md:w-3/5 lg:w-3/5 xl:w-1/3 mx-auto rounded-t-lg md:rounded-lg z-100 overflow-y-auto max-h-full">
+          <div className="modal-content text-left">
+            <div className="flex w-full flex-row-reverse">
+              <div
+                className=" mt-1 mr-4 p-2 hover:bg-gray-200 hover:text-black rounded-lg cursor-pointer"
+                onClick={() => setShowOnboardingModal(false)}
+              >
+                <AiOutlineClose className="" />
+              </div>
+            </div>
+            <div className="px-6 pb-6">
+              <div className="  text-center font-semibold text-[#184061] ">
+                Earn a Canadian Qualification and Work Experience.
+              </div>
+              <div className=" text-center text-sm pt-2 w-full">
+                Programs in Vancouver and Halifax.
+              </div>
+
+              <div className="py-1">
+                <label
+                  htmlFor="email"
+                  className="text-xs 
+         "
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  value={student.email}
+                  onChange={handleChange}
+                  className="w-full rounded-md text-black bg-transparent placeholder-white text-sm border focus:outline-none focus:ring-0 focus:border-bloow-blue "
+                  placeholder="Enter Email"
+                  required
+                />
+              </div>
+
+              <div className="py-1">
+                <label
+                  htmlFor="firstName"
+                  className="text-xs 
+         "
+                >
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  value={student.firstName}
+                  onChange={handleChange}
+                  name="firstName"
+                  className="w-full rounded-md text-black border placeholder-white text-sm  focus:outline-none focus:ring-0 focus:border-bloow-blue bg-transparent"
+                  placeholder="Enter First Name"
+                  required
+                />
+              </div>
+              <div className="py-1">
+                <label
+                  htmlFor="lastName"
+                  className="text-xs 
+         "
+                >
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  name="lastName"
+                  value={student.lastName}
+                  onChange={handleChange}
+                  className="w-full rounded-md text-black border placeholder-white text-sm  focus:outline-none focus:ring-0 focus:border-bloow-blue bg-transparent"
+                  placeholder="Enter Last Name"
+                  required
+                />
+              </div>
+              <div className="py-1">
+                <label
+                  htmlFor="phone"
+                  className="text-xs 
+         "
+                >
+                  Phone Number
+                </label>
+                <input
+                  type="text"
+                  name="phone"
+                  value={student.phone}
+                  onChange={handleChange}
+                  className="w-full rounded-md text-black border placeholder-white text-sm  focus:outline-none focus:ring-0 focus:border-bloow-blue bg-transparent"
+                  placeholder="Enter Last Name"
+                  required
+                />
+              </div>
+
+              <button
+                onClick={uploadstudent}
+                className=" bg-[#184061] text-white mt-4 w-1/2 mx-auto text-center flex justify-center items-center rounded-md px-4 py-1"
+              >
+                {(loading && <Spinner />) || <div className="">Register</div>}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default OnboardingModal;
